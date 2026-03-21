4f568b605ee8cf0ed1733b2aff25a1ddd3101579
Danellalyse <danellajones1@gmail.com>
2026-03-21 19:58:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const totalImages = 31; // change this if you have more or fewer
 
 for (let i = 1; i <= totalImages; i++) {
     const img = document.createElement('img');
-    img.src = `images/shaza${i}.jpg`;
+    img.src = `images/shaza${i}.jpeg`;
     img.alt = `Shaza Drum ${i}`;
     galleryGrid.appendChild(img);
 }
