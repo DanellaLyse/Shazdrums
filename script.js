72ec1a285cf5907d9932934f42edeaed4d23a686
Danellalyse <danellajones1@gmail.com>
2026-03-21 20:10:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const galleryGrid = document.getElementById('gallery-grid');
-const totalImages = 31; // change this if you have more or fewer
+const totalImages = 31;
 
 for (let i = 1; i <= totalImages; i++) {
     const img = document.createElement('img');
     img.src = `images/shaza${i}.jpeg`;
     img.alt = `Shaza Drum ${i}`;
+    img.loading = "lazy";
+
+    img.onerror = () => img.remove(); // removes missing images
+
     galleryGrid.appendChild(img);
 }
