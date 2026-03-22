4c209b95f44148d8bc15f67838f6ab7138a1c392
Danellalyse <danellajones1@gmail.com>
2026-03-21 20:40:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const galleryGrid = document.getElementById('gallery-grid');
 const totalImages = 31;
 
 for (let i = 1; i <= totalImages; i++) {
+
+    const card = document.createElement('div');
+    card.className = "gallery-card";
+
     const img = document.createElement('img');
     img.src = `images/shaza${i}.jpeg`;
     img.alt = `Shaza Drum ${i}`;
     img.loading = "lazy";
 
-    img.onerror = () => img.remove(); // removes missing images
+    img.onerror = function () {
+        card.remove();
+    };
 
-    galleryGrid.appendChild(img);
+    card.appendChild(img);
+    galleryGrid.appendChild(card);
 }
